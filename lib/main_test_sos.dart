ddf3f12caf3abcff2f410c122cbf621049ebdf83
lemical07 <icaleladio@gmail.com>
2026-07-29 20:49:00 -0600

    chore: eliminar archivo temporal de prueba

--- lib/main_test_sos.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:proyecto_alba_guatemala/screens/adulto_mayor/sos_activo_screen.dart';
-
-void main() {
-  runApp(const MaterialApp(home: SosActivoScreen()));
-}
